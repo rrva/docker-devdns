0838efa584fd6777bd0c674c60c6167263cbc634
Ragnar Rova <ragnar.rova@gmail.com>
2015-04-04 13:53:27 +0200

    Listen to docker events
    Querying docker each time was chosen on purpose
    to minimize caching/stale data problems.
    
    But this does not work well performance-wise
    even for smaller setups.
    
    Listen to docker start events instead
    and maintain a local cache of container
    ip addresses instead

--- docker-devdns.go
+++ docker-devdns.go
@@ -1,384 +1,320 @@
 package main
 
 import (
-	"crypto/tls"
-	"errors"
-	"flag"
-	"github.com/miekg/dns"
-	"github.com/rrva/dockerclient"
-	"github.com/wunderlist/ttlcache"
-	"log"
-	"net"
-	"net/http"
-	"net/url"
-	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-	"time"
+    "flag"
+    "github.com/rrva/dockerclient"
+    "log"
+    "os"
+    "strings"
+    "github.com/miekg/dns"
+    "net"
+    "os/signal"
+    "syscall"
 )
 
+
+type myDockerClient interface {
+    StartMonitorEvents(cb dockerclient.Callback, ec chan error, args ...interface{})
+    ListContainers(all bool, size bool, filters string) ([]dockerclient.Container, error)
+    InspectContainer(id string) (*dockerclient.ContainerInfo, error)
+    Version() (*dockerclient.Version, error)
+}
+
+
 func registerShutdownSignalHandlers() {
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
-	for s := range sigChan {
-		log.Fatalf("Signal (%s) received, shutting down", s)
-	}
+    sigChan := make(chan os.Signal, 1)
+    signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+    for s := range sigChan {
+        log.Fatalf("Signal (%s) received, shutting down", s)
+    }
 }
 
-func matches(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
+func cb(event *dockerclient.Event, errors chan error, args ...interface{}) {
+    if (event.Status == "start") {
+        info, err := docker.InspectContainer(event.Id)
+        if err != nil {
+            log.Fatalf("Err: %s", err)
+        } else {
+            name := strings.TrimPrefix(info.Name, "/")
+            log.Printf("%s=%s", name, info.NetworkSettings.IpAddress)
+            nameCache[name] = info.NetworkSettings.IpAddress
+            nameCache[info.NetworkSettings.IpAddress] = name
+
+        }
+    }
+    if (event.Status == "die") {
+        info, err := docker.InspectContainer(event.Id)
+        if err != nil {
+            log.Fatalf("Err: %s", err)
+        } else {
+            name := strings.TrimPrefix(info.Name, "/")
+            delete(nameCache, name)
+            delete(nameCache, info.NetworkSettings.IpAddress)
+
+        }
+    }
 }
 
-type lookupResponse struct {
-	response *string
-	err      error
-}
 
-type lookupFn func(string) (*string, error)
+var docker myDockerClient
 
-func lookupWithTimeout(n string, t time.Duration, lookup lookupFn) (*string, error) {
-
-	value, exists := localLookupResponseCache.Get(n)
-	if exists {
-		return &value, nil
-	}
-
-	errStr, hasError := localLookupErrorsCache.Get(n)
-	if hasError {
-		return nil, errors.New(errStr)
-	}
-
-	lookupResponses := make(chan lookupResponse)
-	go func(n string) {
-		name, err := lookup(n)
-		lookupResponses <- lookupResponse{response: name, err: err}
-	}(n)
-
-	timeouts := make(chan bool, 1)
-	go func() {
-		<-time.After(t)
-		timeouts <- true
-	}()
-
-	for {
-		select {
-		case r := <-lookupResponses:
-			{
-				if r.err != nil {
-					localLookupErrorsCache.Set(n, r.err.Error())
-				} else {
-					if r.response != nil {
-						localLookupResponseCache.Set(n, *(r.response))
-					}
-				}
-				return r.response, r.err
-			}
-		case <-timeouts:
-			return nil, errors.New("Timeout occurred talking to docker")
-		}
-	}
-}
-
-func findContainerNameByIPAddress(n string) (*string, error) {
-	containers, err := docker.ListContainers(false, false, "")
-	if err != nil {
-		return nil, err
-	}
-	for _, container := range containers {
-		info, _ := docker.InspectContainer(container.Id)
-		if info.NetworkSettings.IpAddress == n {
-			if len(container.Names) > 0 {
-				str := container.Names[0][1:]
-				return &str, nil
-			}
-			return nil, nil
-		}
-	}
-	return nil, nil
-}
-
-func findContainerIPAddressByName(n string) (*string, error) {
-	info, err := docker.InspectContainer(n)
-	if err != nil {
-		return nil, err
-	}
-	if info != nil {
-		return &info.NetworkSettings.IpAddress, nil
-	}
-	return nil, nil
-}
-
-func writeDNSResponse(w dns.ResponseWriter, m *dns.Msg) {
-	err := w.WriteMsg(m)
-	if err != nil {
-		log.Printf("Failed writing DNS response: %s", err)
-	}
-}
-
-func myselfAsResolverErrorResponse(w dns.ResponseWriter, m *dns.Msg, name string) {
-	log.Printf("Oh my! I'm pointing to myself!")
-	rr := new(dns.A)
-	rr.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 5}
-	rr.A = net.ParseIP("0.0.0.0").To4()
-	m.Answer = append(m.Answer, rr)
-	writeDNSResponse(w, m)
-}
-
-func loopbackSanityCheckResponse(w dns.ResponseWriter, req *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(req)
-
-	for _, question := range req.Question {
-
-		if question.Name == "dockerdns.loopbacktest." {
-			myselfAsResolverErrorResponse(w, m, question.Name)
-			return
-		}
-	}
-
-	writeDNSResponse(w, m)
-}
-
-func localLookupServer(w dns.ResponseWriter, req *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(req)
-
-	for _, question := range req.Question {
-
-		if question.Qtype == dns.TypeA || question.Qtype == dns.TypeAAAA {
-			IPAddresses, err := net.LookupHost(strings.TrimSuffix(question.Name, "."))
-
-			if err != nil {
-				log.Printf("Local lookup error: %s", err)
-				m.SetRcode(req, dns.RcodeServerFailure)
-				writeDNSResponse(w, m)
-				return
-			}
-
-			for _, IPAddress := range IPAddresses {
-				writeIPAddressResponse(&IPAddress, m, question.Name, question.Qtype)
-			}
-		}
-
-	}
-
-	writeDNSResponse(w, m)
+func refreshEntireCache() {
+    log.Printf("Listing all running containers...")
+    containers, err := docker.ListContainers(false, false, "")
+    if err != nil {
+        log.Printf("error listing containers %s", err)
+    }
+    for _, container := range containers {
+        info, _ := docker.InspectContainer(container.Id)
+        for _, name := range container.Names {
+            name = strings.TrimPrefix(name, "/")
+            log.Printf("%s=%s", name, info.NetworkSettings.IpAddress)
+            nameCache[name] = info.NetworkSettings.IpAddress
+            nameCache[info.NetworkSettings.IpAddress] = name
+        }
+    }
 }
 
 func writeNameResponse(m *dns.Msg, ip string, name *string) {
-	rr := new(dns.PTR)
-	str := *name + "." + domain + "."
-	rr.Hdr = dns.RR_Header{Name: ip, Rrtype: dns.TypePTR, Class: dns.ClassINET, Ttl: 5}
-	rr.Ptr = str
-	m.Answer = append(m.Answer, rr)
+    rr := new(dns.PTR)
+    str := *name + "." + domain + "."
+    rr.Hdr = dns.RR_Header{Name: ip, Rrtype: dns.TypePTR, Class: dns.ClassINET, Ttl: 5}
+    rr.Ptr = str
+    m.Answer = append(m.Answer, rr)
 }
 
 func writeIPAddressResponse(IPAddress *string, m *dns.Msg, name string, qtype uint16) {
-	ip := net.ParseIP(*IPAddress)
-	ip4 := ip.To4()
-	if ip4 != nil && qtype == dns.TypeA {
-		rr := new(dns.A)
-		rr.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 5}
+    ip := net.ParseIP(*IPAddress)
+    ip4 := ip.To4()
+    if ip4 != nil && qtype == dns.TypeA {
+        rr := new(dns.A)
+        rr.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 5}
 
-		rr.A = ip4
-		m.Answer = append(m.Answer, rr)
-	} else if ip4 == nil && qtype == dns.TypeAAAA {
-		rr := new(dns.AAAA)
-		rr.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 5}
+        rr.A = ip4
+        m.Answer = append(m.Answer, rr)
+    } else if ip4 == nil && qtype == dns.TypeAAAA {
+        rr := new(dns.AAAA)
+        rr.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 5}
 
-		rr.AAAA = ip.To16()
-		m.Answer = append(m.Answer, rr)
-	}
+        rr.AAAA = ip.To16()
+        m.Answer = append(m.Answer, rr)
+    }
 }
 
-func inAddrArpaToIpAddress(n string) string {
-	ips := strings.Split(strings.Replace(n, ".in-addr.arpa.", "", 1), ".")
-	s := ""
-	last := len(ips)
-	for i := range ips {
-		s = s + ips[last-1-i]
-		if i < last-1 {
-			s = s + "."
-		}
-	}
-	return s
+func inAddrArpaToIPAddress(n string) string {
+    ips := strings.Split(strings.Replace(n, ".in-addr.arpa.", "", 1), ".")
+    s := ""
+    last := len(ips)
+    for i := range ips {
+        s = s + ips[last-1-i]
+        if i < last-1 {
+            s = s + "."
+        }
+    }
+    return s
 }
 
-func reverseLookupServer(w dns.ResponseWriter, req *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(req)
 
-	for _, question := range req.Question {
-
-		if question.Qtype != dns.TypePTR {
-			continue
-		}
-
-		ip := inAddrArpaToIpAddress(question.Name)
-
-		name, err := lookupWithTimeout(ip, 5*time.Second, findContainerNameByIPAddress)
-
-		if err != nil {
-			log.Printf("Reverse container lookup error for %s: %s", question.Name, err)
-			m.SetRcode(req, dns.RcodeServerFailure)
-			writeDNSResponse(w, m)
-			return
-		}
-
-		if name != nil {
-			writeNameResponse(m, question.Name, name)
-		} else {
-			m.SetRcode(req, dns.RcodeRefused)
-			log.Printf("No match found for %s sending empty reply", question.Name)
-		}
-	}
-
-	writeDNSResponse(w, m)
-
+func writeDNSResponse(w dns.ResponseWriter, m *dns.Msg) {
+    err := w.WriteMsg(m)
+    if err != nil {
+        log.Printf("Failed writing DNS response: %s", err)
+    }
 }
 
 func dockerNameLookupServer(w dns.ResponseWriter, req *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(req)
+    m := new(dns.Msg)
+    m.SetReply(req)
 
-	for _, question := range req.Question {
-		rr := new(dns.A)
-		rr.Hdr = dns.RR_Header{Name: question.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 5}
+    for _, question := range req.Question {
+        rr := new(dns.A)
+        rr.Hdr = dns.RR_Header{Name: question.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 5}
+        name := strings.TrimSuffix(question.Name, "."+domain+".")
+        IPAddress, exists := nameCache[name]
 
-		name := "/" + strings.TrimSuffix(question.Name, "."+domain+".")
+        if !exists {
+            log.Printf("Name not found: %s %s", name, IPAddress)
+            writeDNSResponse(w, m)
+            return
+        }
 
-		IPAddress, err := lookupWithTimeout(name, 5*time.Second, findContainerIPAddressByName)
+        writeIPAddressResponse(&IPAddress, m, question.Name, question.Qtype)
 
-		if err != nil {
-			log.Printf("Container lookup error for %s: %s", question.Name, err)
-			callMeMaybe := "127.0.53.53"
-			writeIPAddressResponse(&callMeMaybe, m, question.Name, question.Qtype)
-			writeDNSResponse(w, m)
-			return
-		}
+    }
 
-		if IPAddress != nil {
-			writeIPAddressResponse(IPAddress, m, question.Name, question.Qtype)
-		} else {
-			log.Printf("No match found for %s sending empty reply", question.Name)
-		}
-	}
-
-	writeDNSResponse(w, m)
+    writeDNSResponse(w, m)
 
 }
 
-func checkIfResolverIsMyself() {
 
-	IPAddresses, _ := net.LookupHost("dockerdns.loopbacktest")
+func localLookupServer(w dns.ResponseWriter, req *dns.Msg) {
+    m := new(dns.Msg)
+    m.SetReply(req)
 
-	if IPAddresses != nil && IPAddresses[0] == "0.0.0.0" {
-		log.Fatal("Configuration error detected, I am pointing to myself as resolver, that won't work")
-	}
+    for _, question := range req.Question {
+
+        if question.Qtype == dns.TypeA || question.Qtype == dns.TypeAAAA {
+            IPAddresses, err := net.LookupHost(strings.TrimSuffix(question.Name, "."))
+
+            if err != nil {
+                log.Printf("Local lookup error: %s", err)
+                m.SetRcode(req, dns.RcodeServerFailure)
+                writeDNSResponse(w, m)
+                return
+            }
+
+            for _, IPAddress := range IPAddresses {
+                writeIPAddressResponse(&IPAddress, m, question.Name, question.Qtype)
+            }
+        }
+
+    }
+
+    writeDNSResponse(w, m)
+}
+
+func reverseLookupServer(w dns.ResponseWriter, req *dns.Msg) {
+    m := new(dns.Msg)
+    m.SetReply(req)
+
+    for _, question := range req.Question {
+
+        if question.Qtype != dns.TypePTR {
+            continue
+        }
+
+        ip := inAddrArpaToIPAddress(question.Name)
+
+        name, exists := nameCache[ip]
+
+        if exists {
+            writeNameResponse(m, question.Name, &name)
+        } else {
+            m.SetRcode(req, dns.RcodeRefused)
+            log.Printf("No match found for %s sending empty reply", question.Name)
+        }
+    }
+
+    writeDNSResponse(w, m)
 
 }
+
 
 func dnsStarted() {
-	log.Printf("DNS server started")
-
-	go checkIfResolverIsMyself()
+    log.Printf("DNS server started")
+    if startedChan != nil {
+        startedChan <- true
+    }
 }
 
 func startDNS(addr string, errors chan error) {
 
-	log.Printf("Trying to listen on %s", addr)
-	server := &dns.Server{Addr: addr, Net: "udp", NotifyStartedFunc: dnsStarted}
+    log.Printf("Trying to listen on %s", addr)
+    server := &dns.Server{Addr: addr, Net: "udp", NotifyStartedFunc: dnsStarted}
 
-	err := server.ListenAndServe()
+    err := server.ListenAndServe()
 
-	if err != nil {
-		errors <- err
-	}
+    if err != nil {
+        errors <- err
+    }
 }
 
-func newHTTPClient(host string, tlsConfig *tls.Config, timeout time.Duration) (*http.Client, *url.URL, error) {
-	u, err := url.Parse(host)
-	if err != nil {
-		return nil, nil, err
-	}
 
-	httpTransport := &http.Transport{
-		TLSClientConfig:       tlsConfig,
-		ResponseHeaderTimeout: timeout,
-	}
-
-	switch u.Scheme {
-	default:
-		httpTransport.Dial = func(proto, addr string) (net.Conn, error) {
-			return net.DialTimeout(proto, addr, timeout)
-		}
-	case "tcp":
-		u.Scheme = "http"
-	case "unix":
-		socketPath := u.Path
-		unixDial := func(proto, addr string) (net.Conn, error) {
-			return net.DialTimeout("unix", socketPath, timeout)
-		}
-		httpTransport.Dial = unixDial
-		u.Scheme = "http"
-		u.Host = "unix.sock"
-		u.Path = ""
-	}
-	return &http.Client{Transport: httpTransport}, u, nil
+func myselfAsResolverErrorResponse(w dns.ResponseWriter, m *dns.Msg, name string) {
+    log.Printf("Oh my! I'm pointing to myself!")
+    rr := new(dns.A)
+    rr.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 5}
+    rr.A = net.ParseIP("0.0.0.0").To4()
+    m.Answer = append(m.Answer, rr)
+    writeDNSResponse(w, m)
 }
 
-var docker *dockerclient.DockerClient
+func loopbackSanityCheckResponse(w dns.ResponseWriter, req *dns.Msg) {
+    m := new(dns.Msg)
+    m.SetReply(req)
+
+    for _, question := range req.Question {
+
+        if question.Name == "dockerdns.loopbacktest." {
+            myselfAsResolverErrorResponse(w, m, question.Name)
+            return
+        }
+    }
+
+    writeDNSResponse(w, m)
+}
+
 var domain string
-var localLookupResponseCache *ttlcache.Cache
-var localLookupErrorsCache *ttlcache.Cache
+var nameCache map[string]string
+var startedChan chan bool
+
+func shutdownOnMonitorErrors(monitorErrors chan error) {
+    err := <-monitorErrors
+    log.Fatalf("Failed listening to docker events: %s", err)
+}
 
 func main() {
+    domain := *flag.String("domain", "dev", "domain")
 
-	domain = *flag.String("domain", "dev", "domain")
+    defaultDockerHost := os.Getenv("DOCKER_HOST")
+    if defaultDockerHost == "" {
+        defaultDockerHost = "tcp://docker:2375"
+    }
+    var dockerHost = *flag.String("docker-host", defaultDockerHost, "docker host url, or set DOCKER_HOST")
+    var listenAddr = *flag.String("listen-addr", ":53", "Listen address for DNS")
+    var otherLookupsToLocalResolver = *flag.Bool("local-resolver", true, "Perform local gethostbyname queries for other domains")
+    flag.Parse()
 
-	localLookupResponseCache = ttlcache.NewCache(time.Second)
-	localLookupErrorsCache = ttlcache.NewCache(time.Second)
 
-	defaultDockerHost := os.Getenv("DOCKER_HOST")
-	if defaultDockerHost == "" {
-		defaultDockerHost = "tcp://docker:2375"
-	}
-	var dockerHost = *flag.String("docker-host", defaultDockerHost, "docker host url, or set DOCKER_HOST")
-	var listenAddr = flag.String("listen-addr", ":53", "Listen address for DNS")
-	var otherLookupsToLocalResolver = *flag.Bool("local-resolver", true, "Perform local gethostbyname queries for other domains")
-	flag.Parse()
+    log.Printf("Resolving %s domain against docker container names at %s", domain, dockerHost)
 
-	var err error
-	httpClient, url, err := newHTTPClient(dockerHost, nil, 100*time.Millisecond)
-	if err != nil {
-		log.Fatalf("Got error %s", err)
-	}
-	docker, err = dockerclient.NewDockerClientFromHttpClient(url, httpClient, nil)
+    var err error
 
-	_, err = docker.Version()
-	if err != nil {
-		log.Fatalf("Failed reading docker version: %s specify host with -docker-host", err)
-	}
+    docker, err = dockerclient.NewDockerClient(dockerHost, nil)
+    if err != nil {
+        log.Fatalf("Failed creating docker client: %s", err)
+    }
+    _, err = docker.Version()
+    if err != nil {
+        log.Fatalf("Failed reading docker version: %s specify host with -docker-host", err)
+    }
 
-	go registerShutdownSignalHandlers()
+    mymain(listenAddr, otherLookupsToLocalResolver, docker, domain, nil)
+}
 
-	log.Printf("Resolving %s domain against docker container names at %s", domain, dockerHost)
-	if otherLookupsToLocalResolver {
-		log.Println("Resolving other domains through system resolver")
-		dns.HandleFunc(".", localLookupServer)
-		dns.HandleFunc("loopbacktest", loopbackSanityCheckResponse)
-	}
-	dns.HandleFunc(domain, dockerNameLookupServer)
-	dns.HandleFunc("arpa", reverseLookupServer)
+func mymain(listenAddr string, otherLookupsToLocalResolver bool, mydocker myDockerClient, mydomain string, started chan bool) {
 
-	dnsListenErrors := make(chan error)
-	go startDNS(*listenAddr, dnsListenErrors)
-	err = <-dnsListenErrors
-	log.Fatalf("Got error %s", err)
+
+    docker = mydocker
+    domain = mydomain
+
+    startedChan = started
+    nameCache = make(map[string]string)
+
+
+    dns.HandleFunc("in-addr.arpa", reverseLookupServer)
+
+
+    if otherLookupsToLocalResolver {
+        log.Println("Resolving other domains through system resolver")
+        dns.HandleFunc(".", localLookupServer)
+        dns.HandleFunc("loopbacktest", loopbackSanityCheckResponse)
+    }
+
+    dns.HandleFunc(domain, dockerNameLookupServer)
+
+
+    monitorErrors := make(chan error)
+    docker.StartMonitorEvents(cb, monitorErrors)
+
+    dnsListenErrors := make(chan error)
+
+    go startDNS(listenAddr, dnsListenErrors)
+    go registerShutdownSignalHandlers()
+    go refreshEntireCache()
+    go shutdownOnMonitorErrors(monitorErrors)
+
+    err := <-dnsListenErrors
+
+    log.Print(err)
 }
